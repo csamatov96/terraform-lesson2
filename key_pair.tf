311512fd2931d911e2bd530a5e5ecc2e4c992e11
csamatov96 <csamatov96@gmail.com>
2019-12-19 17:28:04 -0600

    changes

--- key_pair.tf
+++ key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "key_resource" { #
   key_name   = "Bastion_key" #
-  public_key = "${file("~/.ssh/id_rsa.pub")}" #read a file to get a value of that file 
+  public_key = file("~/.ssh/id_rsa.pub") #read a file to get a value of that file 
 }
