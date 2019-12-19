9bd266a15752c2abec337d42aff668ab9c4ddfa1
csamatov96 <csamatov96@gmail.com>
2019-12-19 17:47:16 -0600

    changes

---
+++ state-lock.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "s3" {
+        dynamodb_table = "infrastructure_state_lock" #
+        bucket = "backend-lesson2-state" #
+        region = "us-east-1" #
+        key = "infrastructure" #was taken from terraform.tfstate
+    }
+}
