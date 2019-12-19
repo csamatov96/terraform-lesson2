8f66022e519f339ea81946e92d9ef749436e1e24
csamatov96 <csamatov96@gmail.com>
2019-12-19 17:16:24 -0600

    custom_sec_group

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
     ami = var.ami #
     instance_type = var.instance_type #
     key_name = aws_key_pair.key_resource.key_name #full path to NAME
-    security_groups = [aws_security_group.my_custom_sec_group.name] # 
+    security_groups = [aws_security_group.custom_sec_group.name] # 
     user_data = "${file("userdata.sh")}" #
     count = "1"
 
